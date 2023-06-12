8a050a1a61b50cc9d894557740ddc29b36a4b079
Troy Lopez <43734061+TLop503@users.noreply.github.com>
2023-06-12 11:27:17 -0700

    added comments

--- FINAL_PROJECT.ino
+++ FINAL_PROJECT.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_CircuitPlayground.h>
 
+//vars for interrupts, pins
 volatile bool switchFlag;
 volatile bool buttonFlagL;
 volatile bool buttonFlagR;
@@ -10,11 +11,9 @@ bool switchState;
 
 
 void setup() {
+  //boilerplate
   CircuitPlayground.begin(10);
-  
   Serial.begin(9600);
-
-  //Set up interrupt pins
   pinMode(inSwitchPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(inSwitchPin), switchInt, CHANGE);
   pinMode(inButtonPinL, INPUT_PULLDOWN);
@@ -23,6 +22,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(inButtonPinR), buttRInt, FALLING);
 }
 
+//ISRs
 void switchInt(){
   switchFlag = !switchFlag;
 }
@@ -33,19 +33,24 @@ void buttRInt(){
   buttonFlagR = true;
 }
 
+//gobal vars used throughout loop()
+//There is a better way to do this, but diminishing returns
 float speed = 1.00;
 bool isCorrect = true;
 int goal;
 bool color;
 
+//resets flags from ISRs
 void resetFlags(){
   buttonFlagL = false;
   buttonFlagR = false;
 }
 
-
+//Picks a random light and sets it to red or blue
 void generateGoal(){
   goal = random(10);
+
+  //I could probably just set color to temp
   int temp = random(2);
   if (temp == 0){
     color = true;
@@ -55,7 +60,7 @@ void generateGoal(){
   }
 }
 
-
+//sets goal to it's color, and the current position of the light to white
 void generateLights(int goal, int index){
   CircuitPlayground.clearPixels();
   if (color) {
@@ -67,29 +72,39 @@ void generateLights(int goal, int index){
   CircuitPlayground.setPixelColor(index, 255, 255, 255);
 }
 
-
+//this is definitely unoptimized but cest la vie
 void loop() {
+  //If the board is "on" play the game
   if (switchFlag) {
+    //if the player clicked the right button at the right time set a new goal
     if (isCorrect) {
       generateGoal();
       isCorrect = false;
     }
+
+    //otherwise continue checking for correct button
     else {
+      
+      //goes through loop counter clockwise
       for (int index = 0; index < 10; index++) {
         generateLights(goal, index);
+        //if you clicked the correct button and matched the color
+        //not OR is inclusive so clicking both buttons at the same time also works, which is nice for testing
         if (((color && buttonFlagL) || (!color && buttonFlagR)) && (index == goal)) {
           speed = 1.5 * speed;
-          CircuitPlayground.playTone(700,100);
+          CircuitPlayground.playTone(700,100); //somewhat happy beep
           isCorrect = true;
           resetFlags();
         }
         else if (buttonFlagL || buttonFlagR) {
-          CircuitPlayground.playTone(300,100);
+          CircuitPlayground.playTone(300,100);  //somewhat unhappy beep
           speed = 1;
           resetFlags();
         } 
         delay(500 / speed);
       }
+
+      //and then clockwise
       for (int index = 9; index > -1; index--) {
         generateLights(goal, index);
         if (((color && buttonFlagL) || (!color && buttonFlagR)) && (index == goal)) {
@@ -107,6 +122,8 @@ void loop() {
       }
     }
   }
+
+  //if board is off just clear pixels and reset speed.
   else {
     CircuitPlayground.clearPixels();
     speed = 1;
